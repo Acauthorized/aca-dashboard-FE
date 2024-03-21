763c8f0feeecbee615aeccf97d645164fdbee66c
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-03-21 16:53:42 +0300

    add social links

--- redux/socket/context.js
+++ redux/socket/context.js
@@ -17,9 +17,7 @@ const dev = "http://localhost:3300";
 export const StateContextProvider = ({ children }) => {
   const socket = io(
     pro,
-  //   {
-  //     transports: ['websocket']
-  //  }
+ 
      { path: "/socket.io", transports: ["websocket"] },
     {
       reconnection: false,
