275dd5f06d2afca1bfdfb0f9d797366797291002
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-03-21 21:26:50 +0300

    add

--- pages/admin/customer/index.jsx
+++ pages/admin/customer/index.jsx
@@ -237,7 +237,7 @@ export default function CustomerList({}) {
     setNote("");
     setNote("");
 
-    socket.sockets.emit("status-customer", status);
+    socket.emit("status-customer", status);
   };
 
   const [custpage, setcustPage] = useState(0);
@@ -515,7 +515,7 @@ export default function CustomerList({}) {
 
   const ExecuteSocket = (data) => {
     console.log("HHIUHHIAHSH", data);
-    socket.sockets.emit("search", data);
+    socket.emit("search", data);
   };
 
   return (
